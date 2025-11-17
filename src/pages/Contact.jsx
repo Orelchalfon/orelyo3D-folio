422c24afb750a4d7352e212518521afaeb20cf60
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-11-17 13:18:56 +0200

    Remove .env file and update Contact component to use local environment variables for EmailJS integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -27,8 +27,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.local.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.local.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Orel Chalfon",
@@ -36,7 +36,7 @@ const Contact = () => {
           to_email: "codeingodmode@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.local.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
